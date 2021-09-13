ef95f81a5f2a60c0b80af41f1e60c52b6a870746
Huynh <phihuynh120120@gmail.com>
2021-09-13 21:08:26 +0700

    add

--- hci-project/src/App.js
+++ hci-project/src/App.js
@@ -10,6 +10,7 @@ function App() {
                     Edit <code>src/App.js</code> and save to reload.
                 </p>
                 <h2>Hello world</h2>
+
                 <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
                     Learn React
                 </a>
